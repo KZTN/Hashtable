1b3a2058ec6248b7af1c3eac4047161c9c21e336
KZTN <KZTNdev@gmail.com>
2019-10-10 10:17:46 -0300

    Added delete function to HashMap class(needs treat last list element)

--- App2.java
+++ App2.java
@@ -9,14 +9,30 @@ public class App2 {
         MapaHash mapa = new MapaHash();
         do {
             System.out.println("Digite uma chave");
-            texto = teclado.next();
-            chave = Integer.parseInt(texto);
+            chave = Integer.parseInt(teclado.next());
             System.out.println("Digite um valor");
             texto = teclado.next();
             r = new Registro(chave, texto);
             mapa.put(r);
-            System.out.println("Continua? (0 para abortar, 1 para continuar)");
+            System.out.println("Continua? (0: Aborta, 1: Continua)");
             continua = Integer.parseInt(teclado.next());
         } while(continua != 0);
+        System.out.println("===================");
+        do {
+            System.out.println("Digite uma chave");
+            chave = Integer.parseInt(teclado.next());
+            r = mapa.get(chave);
+            if (r != null) {
+                System.out.println("Registro Encontrado! = "+r.getValue());
+                System.out.println("Deseja remover deletar este registro?(1: Sim, 0: Não)");
+                int op = Integer.parseInt(teclado.next());
+                if(op == 1) {
+                    mapa.del(r.getKey());
+                    System.out.println("Registro removido com sucesso!");
+                }
+            } else {
+                System.out.println("Registro inexistente");
+            }
+        } while(chave != -1);
     }
 }
--- MapaHash.java
+++ MapaHash.java
@@ -21,7 +21,7 @@ public class MapaHash {
                 if (reg.getProximo() == null) { // se nada foi encontrado, lista no último elemento = null
                     if(reg.getKey() == registro.getKey()) {
                         reg.setValue(registro.getValue()); // apenas substitui o valor
-                        System.out.println("Registro existente, atualizando valor do existente");
+                        System.out.println("Registro existente, atualizando valor no último elemento existente");
                         return;
                     }
                 }
@@ -35,16 +35,50 @@ public class MapaHash {
         r.setKey(key);
         int posicao = r.hashCode();
         Registro resultado = valores[posicao]; // tento encontrar o registro
-        if(resultado.getKey() == key) { // é o que estou procurando?
+        if(resultado != null && resultado.getKey() == key) { // é o que estou procurando?
             return resultado;
         } else {
             while(resultado != null) { // enquanto tiver gente na lista
-                resultado.getProximo(); // vou para o proximo
+                resultado = resultado.getProximo(); // vou para o proximo
                 if(resultado != null &&  resultado.getKey() == key) { // e o cara?
                     return resultado; // se for, retorno ele
                 } 
             }
         }
         return null; // registro não encontrado
+    }
+    public void del(int key) {
+        Registro r = new Registro();
+        r.setKey(key);
+        int hashCode = r.hashCode();
+        if(valores[hashCode] != null && valores[hashCode].getProximo() == null) {
+            System.out.println(valores[hashCode].getValue()+"Foi deletado da lista!");
+            valores[hashCode] = null;
+        } else {
+            Registro reg = valores[hashCode];
+            Registro last = null;
+            while (reg.getProximo() != null) { // se ele esta no meio da lista
+                if (reg.getKey() == r.getKey()) { // o registro que quero inserir  é o mesmo que já existe?
+                    System.out.println(reg.getValue() + "Foi deletado da lista!");
+                    Registro regAux = reg.getProximo();
+                    reg.setValue(regAux.getValue());
+                    reg.setKey(regAux.getKey());
+                    reg.setProximo(regAux.getProximo()); // apenas substitui o valor
+                    return;
+                }
+                last = reg;
+                reg = reg.getProximo();
+            }
+            if(reg.getKey() == r.getKey() && reg.getProximo() == null && last != null){
+                System.out.println(reg.getValue()+"Foi deletado da lista!");
+                System.out.println("Este e o ultimo elemento da lista btw");
+                last.setProximo(null);
+            }
+                //reg = reg.getProximo();
+                // if(reg.getKey() == r.getKey() && reg.getProximo() == null){
+                //     System.out.println(reg.getValue()+"Foi deletado da lista!");
+                //     System.out.println("Este e o ultimo elemento da lista btw");
+                // }
+            }
     }
 }
